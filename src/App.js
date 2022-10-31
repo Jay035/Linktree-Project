d77970a4f3c55d72def1064fc80ba5c16b4f2650
Codexjay <jaydencolins08@gmail.com>
2022-10-31 10:03:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Users from './components/Users';
+import About from './components/About';
+import ErrorPage from './components/ErrorPage';
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+export const Data = async () => {
+    return[
+        {
+            title: "Twitter Link",
+            link: "https:"
+        },
+        {
+            title: "Zuri Team",
+            link: "https:"
+        },
+        {
+            title: "Zuri Books",
+            link: "https:"
+        },
+        {
+            title: "Python Books",
+            link: "https:"
+        },
+        {
+            title: "Background Check for Coders",
+            link: "https:"
+        },
+        {
+            title: "Design Books",
+            link: "https:"
+        },
+        
+    ]
+}
