477f630c7c6a7596e1c116bb75b7db7e2e23e5bc
Codexjay <jaydencolins08@gmail.com>
2022-11-01 22:58:17 +0100

    changed some css styles

--- src/components/data.js
+++ src/components/data.js
@@ -5,42 +5,42 @@ export const Data = async () => {
             title: "Twitter Link",
             link: "https://twitter.com/Chibuike035",
             // aos style
-            aosStyle: "fade-down-left"
+            aosStyle: "fade-left"
         },
         {
             id: "btn__zuri",
             title: "Zuri Team",
             link: "https://training.zuri.team/",
             // aos style
-            aosStyle: "fade-down-right"
+            aosStyle: "fade-right"
         },
         {
             id: "books",
             title: "Zuri Books",
             link: "http://books.zuri.team",
             // aos style
-            aosStyle: "fade-down-left"
+            aosStyle: "fade-left"
         },
         {
             id: "book__python",
             title: "Python Books",
             link: "https://books.zuri.team/python-for-beginners?ref_id=%22CodexJay%27",
             // aos style
-            aosStyle: "fade-down-right"
+            aosStyle: "fade-right"
         },
         {
             id: "pitch",
             title: "Background Check for Coders",
             link: "https://background.zuri.team",
             // aos style
-            aosStyle: "fade-down-left"
+            aosStyle: "fade-left"
         },
         {
             id: "book__design",
             title: "Design Books",
             link: "https://books.zuri.team/design-rules",
             // aos style
-            aosStyle: "fade-down-right"
+            aosStyle: "fade-right"
         },
         
     ]
